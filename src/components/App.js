c78299f00c97ea7ae7d768a202d699f09ed0ad9c
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-19 15:21:28 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import About from "./About"
 function NavBar() {
   return (
     <nav>
@@ -16,15 +16,12 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
